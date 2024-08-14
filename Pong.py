420ed0cb2d3e86cf927817d1199f724ea6ed1caa
Jonathan Wong <Xmexybutt@gmail.com>
2024-08-13 22:27:48 -0400

    Pong Boilerplate

--- Pong.py
+++ Pong.py
@@ -1,45 +1,21 @@
-import pygame
- 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
- 
-# initialize pygame
+import pygame, sys, random
+
 pygame.init()
-screen_size = (700, 500)
+
+WIDTH, HEIGHT = 1280, 720
  
-# create a window
-screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption("pygame Test")
- 
-# clock is used to set a max fps
-clock = pygame.time.Clock()
- 
-# create a demo surface, and draw a red line diagonally across it
-surface_size = (25, 45)
-test_surface = pygame.Surface(surface_size)
-test_surface.fill(WHITE)
-pygame.draw.aaline(test_surface, RED, (0, surface_size[1]), (surface_size[0], 0))
- 
-running = True
-while running:
+FONT = pg.font.Font("Satoshi-Variable.ttf", int(WIDTH/20))
+
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
+CLOCK = pygame.time.Clock()
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-     
-    #clear the screen
-    screen.fill(BLACK)
-     
-    # draw to the screen
-    # YOUR CODE HERE
-    x = (screen_size[0]/2) - (surface_size[0]/2)
-    y = (screen_size[1]/2) - (surface_size[1]/2)
-    screen.blit(test_surface, (x, y))
- 
-    # flip() updates the screen to make our changes visible
-    pygame.display.flip()
-     
-    # how many updates per second
-    clock.tick(60)
- 
-pygame.quit()
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    CLOCK.tick(300)
+
